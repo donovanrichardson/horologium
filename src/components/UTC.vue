e2767ee38e9907abfdf80c771991236c7f267ca2
donovanrichardson <donovanspqa@gmail.com>
2020-12-29 20:29:06 -0500

    made the UTC come up

---
+++ src/components/UTC.vue
@@ -0,0 +1,26 @@
+<template>
+    <p>
+        {{timestring}}
+    </p>
+</template>
+
+<script>
+import { DateTime } from "luxon";
+
+console.log('datetime?', DateTime.utc());
+// console.log('thedata?', this.data);
+
+export default {
+    name: "UTC",
+    data:function(){
+        return {
+            // time:luxon.DateTime.utc()
+            }
+    },
+    computed: {
+        timestring(){
+            return DateTime.utc().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,19 @@
     <b-button variant='success'>
       Hello World
     </b-button>
+    <UTC/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import UTC from '@/components/UTC.vue'
 
 export default {
   name: 'Home',
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    UTC
+  }
 }
 </script>
