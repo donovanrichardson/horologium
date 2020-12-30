99508e3154473005d469d8e5474c1b2a25716c26
donovanrichardson <donovanspqa@gmail.com>
2020-12-30 18:39:53 -0500

    setting up route query

--- src/views/Horologium.vue
+++ src/views/Horologium.vue
@@ -10,18 +10,36 @@
 </template>
 
 <script>
-import { DateTime } from "luxon";
+import { DateTime, Settings, IANAZone } from "luxon";
 
-console.log('datetime?', DateTime.utc());
+console.log('datetime', DateTime);
 // console.log('thedata?', this.data);
 
 export default {
     created(){
-        this.time = DateTime.utc().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+        console.log("route", this.$route);
+        console.log("zone", IANAZone);
+        // console.log("tz", );
+        if(this.$route.query.zone && IANAZone.isValidZone(this.$route.query.zone)){
+            this.timezone = this.$route.query.zone
+        }else{
+            this.timezone = Settings.defaultZoneName
+        }
+        let timestring = this.$route.query.time
+        if(timestring){
+            this.time = DateTime.fromFormat(timestring,'yyyyMMddHHmmss',{zone:this.timezone})
+            console.log('the toyme mate',this.time);
+        }
+
+        if(!timestring || this.time.invalid){
+            this.time = DateTime.fromObject({zone:this.timezone})
+        }
     },
     name: "Horologium",
     data:function(){
         return {
+                time:null,
+                timezone:null
             }
     },
     computed: {
